79ddd4cbe6831b6b17af6c6f74cf76ed8eb7c024
crippin <miklos.istvan2@gmail.com>
2019-11-06 21:41:29 +0100

    fix

--- src/api/twitch.js
+++ src/api/twitch.js
@@ -1,6 +1,6 @@
 import TwitchClient from 'twitch'
 const Client_ID = '9x983jxyvp99rql42txrgaep1i9fct'
-export let TClient = TwitchClient.withCredentials(Client_ID, 'yy6a3mb6d9rzq8crixt3ukjv05klq3')
+export let TClient = TwitchClient.withCredentials(Client_ID, 'TWITCH_TOKEN_HERE')
 const twitchStreams = require('twitch-get-stream')('9x983jxyvp99rql42txrgaep1i9fct');
 
 export const getHlsUrls = id => twitchStreams.get(id)
